a8cc543515359b53390726d3d770aa204900c75b
smhrd <smhrd@222.102.104.138>
2021-07-26 15:32:58 +0900

    memberSQL문

---
+++ AUTOBUY_2/WebContent/MemberSQL.sql
@@ -0,0 +1,12 @@
+create table member
+(customer_id varchar(10) primary key,
+customer_pw varchar(12) not null,
+nick_name varchar(10) not null,
+store_name varchar(30),
+client_resgist_number varchar(30),
+address varchar(50),
+ceo varchar(10),
+tel varchar(13),
+email varchar(20),
+payment varchar(10),            
+);
