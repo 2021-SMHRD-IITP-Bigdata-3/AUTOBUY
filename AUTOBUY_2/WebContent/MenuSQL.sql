1399a123265a959fa876f43f548767c696b31afa
smhrd <smhrd@222.102.104.138>
2021-07-26 14:39:07 +0900

    수정

--- AUTOBUY_2/WebContent/MenuSQL.sql
+++ AUTOBUY_2/WebContent/MenuSQL.sql
@@ -1,13 +1,18 @@
+drop sequence menu_num
+
 create sequence menu_num
-increament 1
+increment by 1
 start with 1
 
 create table menu(
 menu_num number(20),
 menu_name varchar2(20),
 price number(20),
-
-constraint menu_pk primary key(menu_seq, menu_name)
+constraint menu_pk primary key(menu_num, menu_name)
 )
+
+insert into menu values(menu_num.nextval, 'americano',3000);
+insert into menu values(menu_num.nextval, 'latte',4000);
+insert into menu values(menu_num.nextval, 'vanila_latte',4500);
 
 /
